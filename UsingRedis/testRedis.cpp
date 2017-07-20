768e98e9fcfd956701390612a4383f2e28532bcc
52ai <ieeflsyu@outlook.com>
2017-07-20 15:14:01 +0800

    using Redis

---
+++ UsingRedis/testRedis.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <hiredis/hiredis.h>
+
+int main(int argc, const char **argv){
+
+    struct timeval timeout = {2, 0}; //2s的超时时间
+    //redisContext 是Redis操作对象
+    redisContext *pRedisContext = (redisContext*)redisConnectWithTimeout("127.0.0.1",6379,timeout);
+    if((NULL == pRedisContext) || (pRedisContext->err)){
+        if(pRedisContext){
+            std::cout << "connect error:"<< pRedisContext->errstr << std::endl;
+        }
+        else{
+            std::cout << "connect error:can't allocate redis context." << std::endl;
+        }
+        return -1;
+    }
+    //residReply 是Redis命令回复对象，redis返回的信息保存在redisReply对象中
+    redisReply *pRedisReply = (redisReply*)redisCommand(pRedisContext, "ping");//执行INFO命令
+    std::cout << pRedisReply->str << std::endl;
+    //当多条Redis命令使用同一个redisReply对象时
+    //每一次执行完Redis命令后需要清空redisReply以免对下一次的Redis操作造成影响
+    freeReplyObject(pRedisReply);//释放redisReply空间
+
+    pRedisReply = (redisReply*)redisCommand(pRedisContext, "set city beijing");
+    std::cout << pRedisReply->str << std::endl;
+    freeReplyObject(pRedisReply);
+
+    pRedisReply = (redisReply*)redisCommand(pRedisContext, "get city");//执行INFO命令
+    std::cout << pRedisReply->str << std::endl;
+    freeReplyObject(pRedisReply);//释放redisReply空间
+
+    redisFree(pRedisContext);//断开链接并释放redisContext空间
+    return 0;
+
+}
