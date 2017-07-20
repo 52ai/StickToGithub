19790c851ac299b9709dbbe454680c9936609e90
52ai <ieeflsyu@outlook.com>
2017-07-20 16:05:10 +0800

    using hiredis library

--- UsingRedis/testRedis.cpp
+++ UsingRedis/testRedis.cpp
@@ -16,6 +16,8 @@ int main(int argc, const char **argv){
         }
         return -1;
     }
+
+    /*PING SERVER*/
     //residReply 是Redis命令回复对象，redis返回的信息保存在redisReply对象中
     redisReply *pRedisReply = (redisReply*)redisCommand(pRedisContext, "ping");//执行INFO命令
     std::cout << pRedisReply->str << std::endl;
@@ -23,13 +25,24 @@ int main(int argc, const char **argv){
     //每一次执行完Redis命令后需要清空redisReply以免对下一次的Redis操作造成影响
     freeReplyObject(pRedisReply);//释放redisReply空间
 
+    /*Set a Key*/
     pRedisReply = (redisReply*)redisCommand(pRedisContext, "set city beijing");
     std::cout << pRedisReply->str << std::endl;
     freeReplyObject(pRedisReply);
 
+    /*Try a Get*/
     pRedisReply = (redisReply*)redisCommand(pRedisContext, "get city");//执行INFO命令
     std::cout << pRedisReply->str << std::endl;
     freeReplyObject(pRedisReply);//释放redisReply空间
+
+    /*Get multi elements */
+    pRedisReply = (redisReply*)redisCommand(pRedisContext,"hgetall %s", "user");
+    if(pRedisReply->type == REDIS_REPLY_ARRAY){
+        for(size_t s_i=0;s_i<pRedisReply->elements;s_i++){
+            std::cout << pRedisReply->element[s_i]->str << " ";
+        }
+        std::cout << std::endl;
+    }
 
     redisFree(pRedisContext);//断开链接并释放redisContext空间
     return 0;
